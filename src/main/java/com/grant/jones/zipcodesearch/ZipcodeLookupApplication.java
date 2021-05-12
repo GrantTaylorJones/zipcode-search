8cfa3006c2c392323beb86099428ac389d89d5c7
GranTaylorJones <GrantTaylorJones@gmail.com>
2021-05-12 14:38:57 -0400

    zipcode search implemented

---
+++ src/main/java/com/grant/jones/zipcodesearch/ZipcodeLookupApplication.java
@@ -0,0 +1,13 @@
+package com.grant.jones.zipcodesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodeLookupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodeLookupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grant/jones/zipcodesearch/ZipcodeLookupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grant.jones.zipcodesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodeLookupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
